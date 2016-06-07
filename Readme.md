4734fcfba6f94cf5743175cc7fec8db4a18597e9
Matthew Edmondson <mle@wired.one>
2016-06-07 16:16:45 +0100

    first requirement, a selector filled with countries.

---
+++ Readme.md
@@ -0,0 +1,10 @@
+#Rest Country API Lab/Homework
+
+- Make a select drop down with all the countries,
+- Display the country name, population, capital city of the country that is selected.
+- Persist the last country that was selected
+- Style
+
+Further: Add new functionality. eg
+  - Show bordering countries and their statistics.
+  - Filter select to only show countries in a region, sub-region.
--- public/app.js
+++ public/app.js
@@ -1,4 +1,10 @@
 window.onload = function (){
+  getCountriesAjax();
+  // console.log("hello, or something");
+
+};
+
+getCountriesAjax = function(){
   var url = 'https://restcountries.eu/rest/v1';
   var request = new XMLHttpRequest();
   //console.log(request)
@@ -7,15 +13,32 @@ window.onload = function (){
     if(request.status === 200){
       console.log("got the data");
       //console.log(request.responseText);
-    //refactor  var jsonString = request.responseText;
+      //refactor  var jsonString = request.responseText;
       var countries = JSON.parse(request.responseText);
       var country = countries[0];
       console.log(country);
+      fillCountryArray(countries)
       console.log(country.name);
+      console.log(countries.length);
     }
-
   }
-  console.log("hello, or something");
-
   request.send(null);
+}
+
+fillCountryArray = function(countries){
+  for (country of countries) {
+     var countryNameList = new Array();
+     countryNameList.push(country.name);
+     buildSelector(country.name) //hmm maybe we don't need the array
+  }
+}
+
+buildSelector = function(country){
+ console.log(country);
+ selector = document.getElementById('selector')
+ var o = document.createElement("option")
+ o.innerText = country
+ // var o = document.createElement.
+ // var o = document.createElement.
+ selector.appendChild(o)
 }
